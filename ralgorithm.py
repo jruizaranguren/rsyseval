2fbc65e9d5ee6f565beecba17365c60a4e9bcc5f
Javier Ruiz Aranguren <belitre@gmail.com>
2013-10-30 23:53:09 +0100

    First try for global mean ratings

--- ralgorithm.py
+++ ralgorithm.py
@@ -1,4 +1,12 @@
+from rstructures import *
+
 def global_mean(folds,ratings):
-    pass
+    """Makes predictions using Global mean rating. Does not make recommendation list."""
+    for fold in folds:
+        prediction = ratings.ix[fold.test_ix]
+        prediction.rating = ratings.ix[fold.train_ix].rating.mean()
+        yield Recexec(prediction,None)
+        #prediction = test_ix  fold   train_ix,test_ix
+    
 
 
