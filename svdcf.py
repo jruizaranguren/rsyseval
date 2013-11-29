8558f8b6f176ed83710b6f16a6eaa7fdf8197d14
Javier Ruiz Aranguren <belitre@gmail.com>
2013-11-29 07:32:02 +0100

    First try for user-item mean

--- svdcf.py
+++ svdcf.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 test_pairs=[[2048, 788],[2048, 36955],[2048, 77],[1024, 462],[1024, 393],[1024, 36955],[1024, 77],[1024, 268]]
-
+ass_pairs = [[4035,9741],[4035,114],[4035,278],[4035,275],[4035,7443],[2836,2501],[2836,640],[2836,857],[2836,581],[2836,453],[4282,1422],[4282,8587],[4282,238],[4282,1894],[4282,788],[1222,105],[1222,854],[1222,2501],[1222,786],[1222,36657],[2624,2024],[2624,2164],[2624,36955],[2624,36658],[2624,752]]
 class SvdCf:
     DOC_ID = 'docId'
     USER_ID = 'userId'
@@ -29,8 +29,13 @@ class SvdCf:
             self.baseline = self.ratings.sum().sum() / self.ratings.count().sum()
             self.centered = self.ratings - self.baseline
         else:
-            # User-Item personalized mean
-            pass
+            item_mean = self.ratings.mean(axis=0)
+            r1 = self.ratings - item_mean
+            user_mean = pd.DataFrame(r1.mean(axis=1),index=self.ratings.index)
+            self.baseline = np.zeros(self.ratings.shape)
+            self.baseline += item_mean
+            self.baseline = (self.baseline.T + user_mean).T
+            
         self.centered.fillna(0,inplace=True)
 
     def model(self):
@@ -52,4 +57,5 @@ class SvdCf:
     def load_titles(self,path):
         self.titles = pd.read_csv(path,names=[self.DOC_ID,self.TITLE],index_col=0)
 
+#s = SvdCf(mean_type='last')
 
